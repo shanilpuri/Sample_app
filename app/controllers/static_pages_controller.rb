00068dfd8669d0b42f1044848dcaf42054919a39
Shanil Puri <shanil.puri@joshworkz.com>
2012-08-16 14:00:03 +0530

    Updated Commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     if signed_in?
-      puts "hello world!"
       @micropost  = current_user.microposts.build
       @feed_items = current_user.feed.paginate(:page=> params[:page])
     end
